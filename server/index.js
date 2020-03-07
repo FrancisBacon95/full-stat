7586a1cca997b3af554b1d198765c83f6b45a72f
FrancisBacon95 <francis_bacon@naver.com>
2020-03-07 23:20:29 +0900

    backend 1차정리

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts);
+
+
+//Handle production
+if (process.env.NODE_ENV === 'production') {
+    //static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //handle SPA(single page app.)
+    app.get(/.*/,(req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ server/routes/api/posts.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect(
+        "mongodb+srv://choi:*wogur95*@cluster0-f7sw4.mongodb.net/test?retryWrites=true&w=majority",
+      {useNewUrlParser: true, useUnifiedTopology: true}
+    );
+  
+    return client.db('cluster0').collection('full-stat-db');
+  }
+  
+  module.exports = router;
