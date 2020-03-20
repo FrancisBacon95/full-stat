52f38fe91e1e570129b8b331a9ce4c3ad34c9eae
FrancisBacon95 <franics_bacon@naver.com>
2020-03-21 01:42:08 +0900

    1차 배포 준비 완료

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,11 @@
     <Header/>
     <VueChartJS/>
     <router-view/>
+    <div class='footer'>
+      <p>개발자: 이유원, 최재혁</p>
+      <p>도움 주신 분: 데이터 몬스터즈</p>
+      <p>통계학과를 위한 공모전/대회 종합 웹 애플리케이션</p>
+    </div>
     <!--<Footer/>-->
   </div>
 </template>
@@ -41,5 +46,11 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.footer {
+  height:4.0; 
+  background-color: #17a2b8;
+  color : white;
 }
 </style>
