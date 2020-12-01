b46fda3737204d043bc82975a78b05c5ac85242b
FrancisBacon95 <franics_bacon@naver.com>
2020-12-02 01:27:38 +0900

    asfsdf1

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#">!!!!!!!FuCK-Stat!!!!!!!!!</b-navbar-brand>
+        <b-navbar-brand href="#">!!!!!!!!!FuCK-Stat!!!!!!!!!</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-                <b-nav-item href="#">공모전/대회</b-nav-item>
+                <b-nav-item href="#">공asdf모전/대회</b-nav-item>
                 <b-nav-item href="#">Disabled</b-nav-item>
             </b-navbar-nav>
         </b-collapse>
