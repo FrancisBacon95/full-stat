abc2288ce855a1c63dd4559f38d3000eaca7501e
FrancisBacon95 <franics_bacon@naver.com>
2020-12-01 22:13:45 +0900

    footer, header 수정

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#">Full-Stat</b-navbar-brand>
+        <b-navbar-brand href="#">FuCK-Stat!!!!!!!!!</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
